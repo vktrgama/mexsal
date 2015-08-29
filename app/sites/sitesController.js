03978a6e929dbd306e899a2b1de19e060c3d4836
vktrgama <vktrgama@gmail.com>
2015-08-28 19:23:28 -0700

    Show site details

--- app/sites/sitesController.js
+++ app/sites/sitesController.js
@@ -1,32 +1,36 @@
 app.controller('SitesController', ['$scope','$http','$rootScope', 'superCache',
 	function ($scope, $http, $rootScope, superCache) {	
+		$scope.showSite = false;
 		
 		var cachedSites = superCache.get('mySites');
 		if (cachedSites != null){
 			// get sites from cache
 			$scope.mySites = cachedSites;
 		} else {
-			// get sites from database
-			if (!angular.isUndefined($rootScope.userInfo.email)){
-				$http({
-					method: 'POST',
-					url: 'dal/sitesRepo.php',
-					data: { "Func": "GetSites", "Data": [{"email": $rootScope.userInfo.email }] }
-				})
-				.success(function(data, status, headers, config) {
-					if (data.length){
-						superCache.put('mySites', data);
-						$scope.mySites = data;
-					}
-				})
-				.error(function() {
-					$scope.errorMsg = "No sites found";
-				});
+			if (!angular.isUndefined($rootScope.userInfo)){
+				// get sites from database
+				if (!angular.isUndefined($rootScope.userInfo.email)){
+					$http({
+						method: 'POST',
+						url: 'dal/sitesRepo.php',
+						data: { "Func": "GetSites", "Data": [{"email": $rootScope.userInfo.email }] }
+					})
+					.success(function(data, status, headers, config) {
+						if (data.length){
+							superCache.put('mySites', data);
+							$scope.mySites = data;
+						}
+					})
+					.error(function() {
+						$scope.errorMsg = "No sites found";
+					});
+				}
 			}
 		}
 		
 		$scope.openSiteModal = function (site) {
-			alert(site.Site_Name);
+			$scope.siteSelected = site;
+			$scope.showSite = true;
 		};
 	}
 ]);
